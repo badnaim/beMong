7408cb9cc84d1c16d4c8ddc79bfe1e80fd30edc7
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-16 12:26:05 +0800

    nothing

--- routes/movies-api.js
+++ routes/movies-api.js
@@ -14,6 +14,11 @@ movieRouter.get("/", async (request, response) => {
   }
 });
 
+movieRouter.post("/post", async (req, res) => {
+  console.log("posted");
+  res.send("test");
+});
+
 export default movieRouter;
 
 // module js, common js
